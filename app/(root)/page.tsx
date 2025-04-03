20ede63b244946b79460b7e462c2a179f32a37df
Vikas Vaibhav <sinhaamanas2832439@gmail.com>
2025-04-03 15:20:39 +0530

    Add InterviewCard component and integrate dayjs for date formatting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard';
 import { Button } from '@/components/ui/button';
+import { dummyInterviews } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -33,15 +35,21 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          <p>You haven't taken any interview yet.</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
         </div>
       </section>
 
-      <section className='flex flex-col gap-6 mt-8'>
+      <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
 
-        <div className=''>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
 
+          {/* <p>You haven't taken any interview yet.</p> */}
         </div>
       </section>
     </>
