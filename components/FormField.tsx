73c5958f2a05a335ff3f3507285b044073c6ea50
Vikas Vaibhav <sinhaamanas2832439@gmail.com>
2025-04-03 10:38:41 +0530

    Implement authentication forms and layout components

---
+++ components/FormField.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FormControl, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: 'text' | 'email' | 'password' | 'file'
+}
+
+const FormField = <T extends FieldValues> ({control, name, label, placeholder, type="text"}: FormFieldProps<T>) => {
+    return (
+        <Controller 
+            name={name} 
+            control={control} 
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel className='label'>{label}</FormLabel>
+                    <FormControl>
+                        <Input className='input'  type={type} {...field} placeholder={placeholder} />
+                    </FormControl>
+                     
+                    <FormMessage />
+                </FormItem>
+            )} 
+        />
+    )
+}
+
+export default FormField
